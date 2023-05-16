7c3018d11b6123004e9ebaed95a5b1cce00ba615
Bruno Shiohei <89222572+shioheii@users.noreply.github.com>
2023-05-16 19:17:42 -0300

    Add files via upload

--- projeto2/projeto2/Form1.cs
+++ projeto2/projeto2/Form1.cs
@@ -21,7 +21,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace projeto2
 {
@@ -32,26 +31,27 @@ namespace projeto2
             InitializeComponent();
         }
 
+        bool desenharSalvos = false;
         bool marcarCoordenadas = false;
         bool validarDesenho = false;
         int desenhoSelecionado = 0; // 1 - Reta / 2 - Retangulo / 3 - Circulo / 4 - Elipse / 5 - Trialgulo / 6 - Losango / 7 - Pentagono
         int clicks = 0;
         int[] coordenadas = new int[10];
-        int raio, altura, largura; // Caso da elipse e circulo // Dá pra fazer var local 
+        int raio, altura, largura; // Caso da elipse e circulo
         int indiceCoordenadas = 0;
         int formatoSelecionado = 1;
         float[] formatoLinha = { 1 };
         int espessuraSelecionada = 1;
         int espessura = 1;
         int corSelecionada = 1;
-        Color cor;
-        
+        int[] RGB = new int[3] { 0, 0, 0 };
+
         Color color(int r, int g, int b, PaintEventArgs e)
         {
             return Color.FromArgb(r, g, b);
         }
         
-        Pen criarCaneta(Color cor, float[] fLinha, int espessura, PaintEventArgs e) // Só pra testes manuais
+        Pen criarCaneta(Color cor, float[] fLinha, int espessura, PaintEventArgs e)
         {
             Pen caneta = new Pen(cor, espessura);
             caneta.DashPattern = fLinha;
@@ -70,9 +70,10 @@ namespace projeto2
 
         void retangulo(int[] pontos, Pen caneta, PaintEventArgs e)
         {
-            int[] indices = { 0, 1, 2, 1, 2, 1, 2, 3, 2, 3, 0, 3, 0, 3, 0, 1 };
-            for (int i = 0; i < indices.Length; i+=4)
-                linha(pontos[indices[i]], pontos[indices[i+1]], pontos[indices[i+2]], pontos[indices[i+3]], caneta, e);
+            linha(pontos[0], pontos[1], pontos[2], pontos[1], caneta, e);
+            linha(pontos[2], pontos[1], pontos[2], pontos[3], caneta, e);
+            linha(pontos[2], pontos[3], pontos[0], pontos[3], caneta, e);
+            linha(pontos[0], pontos[3], pontos[0], pontos[1], caneta, e);
         }
 
         void circulo(int[] pontos, Pen caneta, PaintEventArgs e)
@@ -106,6 +107,64 @@ namespace projeto2
                 linha(pontos[indices[i]], pontos[indices[i+1]], pontos[indices[i+2]], pontos[indices[i+3]], caneta, e);
         }
 
+        void definirFormatoLinha()
+        {
+            if (formatoSelecionado == 1)
+                formatoLinha = new float[1] { 1 };
+            else if (formatoSelecionado == 2)
+                formatoLinha = new float[2] { 2, 2 };
+            else if (formatoSelecionado == 3)
+                formatoLinha = new float[2] { 10, 5 };
+            else if (formatoSelecionado == 4)
+                formatoLinha = new float[4] { 10, 3, 2, 3 };
+            else
+                formatoLinha = new float[4] { 20, 4, 10, 4 };
+        }
+
+        void definirCor()
+        {
+            if (corSelecionada == 1)
+                RGB = new int[3] { 0, 0, 0 };
+            else if (corSelecionada == 2)
+                RGB = new int[3] { 255, 255, 255 };
+            else if (corSelecionada == 3)
+                RGB = new int[3] { 127, 127, 127 };
+            else if (corSelecionada == 4)
+                RGB = new int[3] { 195, 195, 195 };
+            else if (corSelecionada == 5)
+                RGB = new int[3] { 136, 0, 21 };
+            else if (corSelecionada == 6)
+                RGB = new int[3] { 185, 122, 87 };
+            else if (corSelecionada == 7)
+                RGB = new int[3] { 237, 28, 36 };
+            else if (corSelecionada == 8)
+                RGB = new int[3] { 255, 174, 201 };
+            else if (corSelecionada == 9)
+                RGB = new int[3] { 255, 127, 39 };
+            else if (corSelecionada == 10)
+                RGB = new int[3] { 255, 201, 14 };
+            else if (corSelecionada == 11)
+                RGB = new int[3] { 255, 242, 0 };
+            else if (corSelecionada == 12)
+                RGB = new int[3] { 239, 228, 176 };
+            else if (corSelecionada == 13)
+                RGB = new int[3] { 34, 177, 76 };
+            else if (corSelecionada == 14)
+                RGB = new int[3] { 181, 230, 29 };
+            else if (corSelecionada == 15)
+                RGB = new int[3] { 0, 162, 232 };
+            else if (corSelecionada == 16)
+                RGB = new int[3] { 153, 217, 234 };
+            else if (corSelecionada == 17)
+                RGB = new int[3] { 63, 72, 204 };
+            else if (corSelecionada == 18)
+                RGB = new int[3] { 112, 146, 190 };
+            else if (corSelecionada == 19)
+                RGB = new int[3] { 163, 73, 164 };
+            else
+                RGB = new int[3] { 200, 191, 231 };
+        }
+
         int getQtdClicksDesenho()
         {
             if (desenhoSelecionado == 1) // Linha
@@ -125,46 +184,6 @@ namespace projeto2
             return -1;
         }
 
-        void salvarDesenhos()
-        {
-            // Desenho // Cor // Formato // Espessura // Raio ou Largura e Altura // Coordenadas
-            MessageBox.Show("Oi");
-
-            String txt = "" + desenhoSelecionado + " " + corSelecionada + " " + formatoSelecionado + " " + espessuraSelecionada + " ";
-            File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", txt);
-
-            if (desenhoSelecionado == 3)
-                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", raio + " ");
-            else if (desenhoSelecionado == 4)
-                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", altura + " " + largura + " ");
-
-            for (int i = 0; i < getQtdClicksDesenho() * 2; i++)
-                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", coordenadas[i] + " ");
-            File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", "" + Environment.NewLine);
-        }
-
-        void definirCor(PaintEventArgs e)
-        {
-            if (corSelecionada == 1)
-                cor = color(0, 0, 0, e);
-            else if (corSelecionada == 2)
-                cor = color(127, 127, 127, e);
-        }
-
-        void definirFormatoLinha(int opc)
-        {
-            if (opc == 1)
-                formatoLinha = new float[1] { 1 };
-            else if (opc == 2)
-                formatoLinha = new float[2] { 2, 2 };
-            else if (opc == 3)
-                formatoLinha = new float[2] { 10, 5 };
-            else if (opc == 4)
-                formatoLinha = new float[4] { 10, 3, 2, 3 };
-            else
-                formatoLinha = new float[4] { 20, 4, 10, 4 };
-        }
-
         void permitirInvalidate()
         {
             if (clicks == getQtdClicksDesenho())
@@ -174,10 +193,15 @@ namespace projeto2
             }
         }
 
-        void desenharPaint(Pen caneta, PaintEventArgs e)
+        void desenharPaint(PaintEventArgs e)
         {
-            if (validarDesenho)
+            if (validarDesenho || desenharSalvos)
             {
+                definirCor();
+                definirFormatoLinha();
+                Color cor = color(RGB[0], RGB[1], RGB[2], e);
+                Pen caneta = criarCaneta(cor, formatoLinha, espessura, e);
+
                 if (desenhoSelecionado == 1)
                     linha(coordenadas[0], coordenadas[1], coordenadas[2], coordenadas[3], caneta, e);
                 else if (desenhoSelecionado == 2)
@@ -193,21 +217,78 @@ namespace projeto2
                 else if (desenhoSelecionado == 7)
                     pentagono(coordenadas, caneta, e);
 
-                salvarDesenhos();
+                if(!desenharSalvos)
+                    salvarDesenhos();
 
                 //marcarCoordenadas = false;
-                //validarDesenho = false;
-                indiceCoordenadas = 0;
                 //tipoDesenho = 0;
+                validarDesenho = false;
+                indiceCoordenadas = 0;
                 clicks = 0;
+                desenharSalvos = true;
+            }
+        }
+
+        void salvarDesenhos()
+        {
+            // Desenho // Cor // Formato // Espessura // Raio ou Largura e Altura // Coordenadas
+
+            String txt = "" + desenhoSelecionado + " " + corSelecionada + " " + formatoSelecionado + " " + espessuraSelecionada + " ";
+            File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", txt);
+
+            if (desenhoSelecionado == 3)
+                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", raio + " ");
+            else if (desenhoSelecionado == 4)
+                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", largura + " " + altura + " ");
+
+            for (int i = 0; i < getQtdClicksDesenho() * 2; i++)
+                File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", coordenadas[i] + " ");
+            File.AppendAllText(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt", "" + Environment.NewLine);
+        }
+
+        void pintarDesenhosSalvos(PaintEventArgs e)
+        {
+            // Desenho // Cor // Formato // Espessura // Raio ou Largura e Altura // Coordenadas
+
+            if (File.Exists(@"C:\\Users\\user\\Documents\\INFORMATICA\\PROJETOS\\ICG\\2 BIMESTRE\\dados.txt"))
+            {
+                String[] desenhos = File.ReadAllLines(@"C:\Users\user\Documents\INFORMATICA\PROJETOS\ICG\2 BIMESTRE\dados.txt");
+
+                if (desenhos.Length > 0)
+                {
+                    foreach (String desenho in desenhos)
+                    {
+                        int i = 0;
+                        String[] dadosDesenho = desenho.Split(' ');
+
+                        desenhoSelecionado = int.Parse(dadosDesenho[i++]);
+                        corSelecionada = int.Parse(dadosDesenho[i++]);
+                        formatoSelecionado = int.Parse(dadosDesenho[i++]);
+                        espessura = int.Parse(dadosDesenho[i++]) * 3;
+
+                        if (desenhoSelecionado == 3)
+                            raio = int.Parse(dadosDesenho[i++]);
+                        else if (desenhoSelecionado == 4)
+                        {
+                            largura = int.Parse(dadosDesenho[i++]);
+                            altura = int.Parse(dadosDesenho[i++]);
+                        }
+
+                        for (int j = 0; j < getQtdClicksDesenho() * 2; j++)
+                            coordenadas[j] = int.Parse(dadosDesenho[i++]);
+
+                        desenharPaint(e);
+                    }
+                }
+                desenharSalvos = false;
             }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            definirCor(e);
-            Pen caneta = criarCaneta(cor, formatoLinha, espessura, e);
-            desenharPaint(caneta, e);
+            desenharPaint(e);
+            if (desenharSalvos)
+                pintarDesenhosSalvos(e);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
@@ -265,7 +346,6 @@ namespace projeto2
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             formatoSelecionado = int.Parse(comboBox1.SelectedItem.ToString());
-            definirFormatoLinha(formatoSelecionado);
         }
 
         // Tipo de espessura
@@ -273,6 +353,106 @@ namespace projeto2
         {
             espessuraSelecionada = int.Parse(comboBox2.SelectedItem.ToString());
             espessura = espessuraSelecionada * 3;
+        }
+
+        private void button8_Click(object sender, EventArgs e) //cor preta
+        {
+            corSelecionada = 1;
+        }
+
+        private void button9_Click(object sender, EventArgs e) //cor branca
+        {
+            corSelecionada = 2;
+        }
+
+        private void button10_Click(object sender, EventArgs e) //cor cinza 
+        {
+            corSelecionada = 3;            
+        }
+
+        private void button11_Click(object sender, EventArgs e) //cor cinza claro
+        {
+            corSelecionada = 4;            
+        }
+
+        private void button12_Click(object sender, EventArgs e) //vermelho escuro
+        {
+            corSelecionada = 5;
+        }
+
+        private void button13_Click(object sender, EventArgs e) //marrom
+        {
+            corSelecionada = 6;            
+        }
+
+        private void button14_Click(object sender, EventArgs e) //vermelho
+        {
+            corSelecionada = 7;            
+        }
+
+        private void button15_Click(object sender, EventArgs e) //rosa
+        {
+            corSelecionada = 8;            
+        }
+
+        private void button16_Click(object sender, EventArgs e) //laranja
+        {
+            corSelecionada = 9;            
+        }
+
+        private void button17_Click(object sender, EventArgs e) //dourado
+        {
+            corSelecionada = 10;            
+        }
+
+        private void button18_Click(object sender, EventArgs e) //amarelo
+        {
+            corSelecionada = 11;            
+        }
+
+        private void button19_Click(object sender, EventArgs e) //amarelo claro
+        {
+            corSelecionada = 12;            
+        }
+
+        private void button20_Click(object sender, EventArgs e) //verde
+        {
+            corSelecionada = 13;            
+        }
+
+        private void button21_Click(object sender, EventArgs e) //lima
+        {
+            corSelecionada = 14;            
+        }
+
+        private void button22_Click(object sender, EventArgs e) //turquesa
+        {
+            corSelecionada = 15;            
+        }
+
+        private void button23_Click(object sender, EventArgs e) //turquesa claro
+        {
+            corSelecionada = 16;            
+        }
+
+        private void button24_Click(object sender, EventArgs e) //índigo
+        {
+            corSelecionada = 17;            
+        }
+
+        private void button25_Click(object sender, EventArgs e) //cinza azulado
+        {
+            corSelecionada = 18;            
+        }
+
+        private void button26_Click(object sender, EventArgs e) //roxo
+        {
+            corSelecionada = 19;            
+        }
+
+        private void button27_Click(object sender, EventArgs e) //lavanda
+        {
+            corSelecionada = 20;            
         }
 
         // Botão do losango
